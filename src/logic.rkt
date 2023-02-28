48601b63512ab95b14506e52d3eafef26725592f
GChacon8 <gabriel.chacon.alfaro@gmail.com>
2023-02-28 00:09:07 -0600

    Funcion generaMatriz
    Se implementa la funcion generaMatriz para dar inicio a un tablero de juego con las dimesiones establecidas.

--- src/logic.rkt
+++ src/logic.rkt
@@ -1,1 +1,16 @@
 #lang racket
+;;Genera una fila de la matriz solicitada, con la cantidad de columnas especificada.
+(define (generaColumnas columnas lista)
+  (cond
+    ((equal? columnas 0) (cons lista '()))
+    (else (generaColumnas (- columnas 1) (cons 0 lista)))))
+
+;;Repite una fila encima de otra hasta saciar la cantidad de filas solicitada.
+(define (generaFilas filas lista matriz)
+  (cond
+    ((equal? filas 0) matriz)
+    (else (generaFilas (- filas 1) lista (append matriz lista)))))
+
+;;Combina el generaColumnas y el generaFilas para crear la matriz
+(define (generaMatriz filas columnas)
+  (generaFilas filas (generaColumnas columnas '()) '()))
