b206537e33fb38b3a72f351cd764f8452a3fa811
Emanuel Marin <emanuel.marin@ncubo.com>
2023-02-28 22:43:09 -0600

    advance in the interface, load board from an array

--- src/gui.rkt
+++ src/gui.rkt
@@ -3,6 +3,52 @@
 (require racket/gui racket/include)
 (include "logic.rkt")
 
+; Variables globales
+(define player-name "")
+(define color-value 1) ; 1 -> blue, 2 -> white
+(define possible-values '(8 9 10 11 12 13 14 15 16))
+(define rows 8)
+(define cols 8)
+(define array '(()))
+
+#|
+Nombre: set-player-name 
+Descripción: Almacena el nombre del jugador que iniciará la partida contra la PC
+Entradas: name ->  Nombre del jugador
+Salida: Nombre almacenado
+|#
+(define (set-player-name name)
+    (set! player-name name))
+
+#|
+Nombre: set-color-value 
+Descripción: Almacena el valor asociado al color de la ficha seleccionada por el jugador
+Entradas: color ->  Valor del color de la ficha
+Salida: Valor del color almacenado
+|#
+(define (set-color-value color)
+    (set! color-value color))
+
+#|
+Nombre: set-rows-cols
+Descripción: Almacena el número de filas y columnas asociadas a las dimensiones del tablero
+Entradas: num-rows -> número de filas
+          num-cols -> número de columnas
+Salida: Dimensiones del tablero almacenadas
+|#
+(define (set-rows-cols num-rows num-cols)
+    (set! rows num-rows)
+    (set! cols num-cols))
+
+#|
+Nombre: set-array
+Descripción: Crea una matriz de rows x cols vacia que representa el tablero al inicio de la partida
+Entradas: No tiene entrradas
+Salida: Matriz vacia con las dimensiones del tablero
+|#
+(define (set-array)
+    (set! array (generate-array rows cols)))
+
 ; Función principal
 #|
 Nombre: 4Line
@@ -60,16 +106,23 @@ Salida: Primer ventana al iniciar el juego
     (new message% [parent top-panel-left]
                   [label "SETTINGS"])
 
+    ; Widget para establecer el nombre del jugador que se enfrentará a la PC
     (define text-field
         (new text-field% [parent top-panel-center]
                          [label "Name:"]
                          [init-value "You"]))
 
+    ; Widget para determinar las dimensiones del tablero
     (define combo-field
         (new combo-field% [parent top-panel-center]
                           [label "Field Size:"]
                           [init-value "Default (8x8)"]
-                          [choices '("Default (8x8)" "Large (16x16)" "Other")]))
+                          [choices '("Default (8x8)" "Large (16x16)" "Other")]
+                          [callback (λ (b e) (on-combo-field))]))
+
+    ; Función que actualiza las dimensiones del tablero
+    (define (on-combo-field)
+        (cond [(equal? (send combo-field get-value) "Other") (board-size-window start-window) (send combo-field set-value (string-append "Other (" (~v rows) "x" (~v cols) ")"))]))
 
     ; Botón para llamar a la ventana de información 
     (new button% [parent top-panel-right]
@@ -94,7 +147,7 @@ Salida: Primer ventana al iniciar el juego
         (new horizontal-panel% [parent center-panel-center] [alignment '(center bottom)]))
 
     (define center-panel-center-right
-        (new horizontal-panel% [parent center-panel-center] [alignment '(right bottom)] [horiz-margin 0]))
+        (new horizontal-panel% [parent center-panel-center] [alignment '(right bottom)]))
 
     (new message% [parent center-panel-left]
                   [label "SELECT YOUR COLOR"])
@@ -107,12 +160,18 @@ Salida: Primer ventana al iniciar el juego
     (new message% [parent center-panel-center-right]
                   [label (read-bitmap "src\\resources\\white-token.png")])   
 
+    ; Widget para determinar con que color de ficha el jugador se enfretara con la PC
     (define radio-box
         (new radio-box% [parent center-panel-center-center]
                         [label ""] 
                         [choices (list "Blue               " "White")]
                         [style '(horizontal)]
-                        [callback (λ (b e) (display "Test"))]))
+                        [callback (λ (b e) (on-radio-box))]))
+
+    ; Función que actualiza el color de la ficha del jugador
+    (define (on-radio-box)
+        (cond [(zero? (send radio-box get-selection)) (set-color-value 1)]
+              [else (set-color-value 2)]))
 
     (define bottom-panel
         (new horizontal-panel% [parent start-panel] [alignment '(center bottom)]))
@@ -121,10 +180,73 @@ Salida: Primer ventana al iniciar el juego
     (new button% [parent bottom-panel]
                  [vert-margin 5]
                  [label "        Start Match        "]
-                 [callback (λ (b e) (on-play-button))])
+                 [callback (λ (b e) (on-play-button (send text-field get-value) (send combo-field get-value)))])
 
-    (define (on-play-button)
+    ; Verifica el nombre del jugador, las dimensiones del tablero y el color de la ficha antes de iniciar la partida
+    (define (on-play-button name dimensions)
+        (validate-name name)
+        (validate-dimensions dimensions)
+        (create-init-board)
         (start-match))
+
+    (define (validate-name name)
+        (cond ((zero? (string-length name)) (set-player-name "You"))
+              (else (set-player-name name))))
+
+    (define (validate-dimensions dimensions)
+        (cond ((equal? "Default (8x8)" dimensions) (set-rows-cols 8 8))
+              ((equal? "Large (16x16)" dimensions) (set-rows-cols 16 16))))
+
+    (define (create-init-board)
+        (set-array)
+        (define target (make-bitmap (* rows 50) (* cols 50)))
+        (define dc (new bitmap-dc% [bitmap target]))
+        
+        (define (draw-board array)
+            (draw-board-aux array 0 0 4 5))
+
+        (define (draw-board-aux array rx ry cx cy)
+            (draw-in-bitmap array rx ry cx cy)
+            (cond ((null? array) (send target save-file "src\\resources\\init-board.png" 'png))
+                    (else (draw-board-aux (cdr array) 0 (+ ry 50) 4 (+ cy 50)))))
+
+        (define (draw-in-bitmap array rx ry cx cy)
+            (cond ((null? array) #f)
+                    (else (draw-in-bitmap-aux (car array) rx ry cx cy))))
+
+        (define (draw-in-bitmap-aux lst rx ry cx cy)
+            (add-cell lst rx ry cx cy)
+            (cond ((null? lst) #f)
+                    (else (draw-in-bitmap-aux (cdr lst) (+ rx 50) ry (+ cx 50) cy))))
+
+        (define (add-cell lst rx ry cx cy)
+            (cond ((null? lst) #f)
+                    ((equal? (car lst) 0) (draw-empty-cell rx ry))
+                    ((equal? (car lst) 1) (draw-blue-token rx ry cx cy))
+                    ((equal? (car lst) 2) (draw-white-token rx ry cx cy))))
+
+        (define (draw-empty-cell rx ry)
+            (send dc set-brush (make-object color% 70 130 180 1.0) 'solid)
+            (send dc set-pen "white" 2 'solid)
+            (send dc draw-rectangle rx ry 50 50))
+
+        (define (draw-blue-token rx ry cx cy)
+            (send dc set-brush (make-object color% 70 130 180 1.0) 'solid)
+            (send dc set-pen "white" 2 'solid)
+            (send dc draw-rectangle rx ry 50 50)
+            (send dc set-brush (make-object color% 0 150 255 1.0) 'solid)
+            (send dc set-pen "black" 1 'solid)
+            (send dc draw-ellipse cx cy 40 40))
+
+        (define (draw-white-token rx ry cx cy)
+            (send dc set-brush (make-object color% 70 130 180 1.0) 'solid)
+            (send dc set-pen "white" 2 'solid)
+            (send dc draw-rectangle rx ry 50 50)
+            (send dc set-brush "white" 'solid)
+            (send dc set-pen "black" 1 'solid)
+            (send dc draw-ellipse cx cy 40 40))
+
+        (draw-board array))
 
 ; Ventana de información
 #|
@@ -156,6 +278,44 @@ Salida: Ventana de información
         
     (send about-dialog show #t))
 
+; Algunas ventanas emergentes
+#|
+Nombre: board-size-window
+Descripción: Ventana emergente para que el jugador pueda establecer la dimensión del tablero
+Entradas: place -> Lugar donde se colocará la ventana
+Salida: Dimension del tablero que puede ser distinta a las de default 
+|#
+(define (board-size-window place)
+    (define board-size (new dialog% [parent place] [label "Board Size"] [width 280] [height 120]))
+    (define pane (new pane% [parent board-size]))
+    (define panel (new vertical-panel% [parent pane] [alignment '(center center)] [spacing 0]))
+    (define top-panel (new horizontal-panel% [parent panel] [alignment '(center top)]))
+    (define bottom-panel (new horizontal-panel% [parent panel] [alignment '(center bottom)]))
+
+    (define choice1 (new choice%
+                    [label "Rows:"]
+                    [parent top-panel]
+                    [choices (list "8" "9" "10" "11" "12" "13" "14" "15" "16")]
+                    [callback (λ (b e) (on-choice1 (send choice1 get-selection)))]))
+
+    (define (on-choice1 selection)
+        (set-rows-cols (list-ref possible-values selection) cols))
+
+    (define choice2 (new choice%
+                    [label "Cols:"]
+                    [parent top-panel]
+                    [choices (list "8" "9" "10" "11" "12" "13" "14" "15" "16")]
+                    [callback (λ (b e) (on-choice2 (send choice2 get-selection)))]))
+
+    (define (on-choice2 selection)
+        (set-rows-cols rows (list-ref possible-values selection)))
+
+    (new button%  [parent bottom-panel]
+                  [label "OK"]
+                  [callback (λ (b e) (send board-size show #f))])
+
+    (send board-size show #t))
+
 ; Ventana de juego
 #|
 Nombre: start-match
@@ -169,23 +329,37 @@ Salida: Ventana de juego
     (define game-window
         (new frame% [parent start-window] [label "4Line"] [width 800] [height 600]))
 
+    ; Principales funciones de la ventana
+
     ; Principal contenedor de la ventana
     (define game-pane
         (new pane% [parent game-window]))
 
     ; Distribución de la ventana
     (define game-panel
-        (new vertical-panel% [parent game-pane] [alignment '(center center)]))
+        (new horizontal-panel% [parent game-pane] [alignment '(center center)]))
     
-    (define label-panel
-        (new horizontal-panel% [parent game-panel] [alignment '(center center)]))
-
-    (define button-panel
-        (new horizontal-panel% [parent game-panel] [alignment '(center bottom)] [vert-margin 5]))
+    (define left-panel
+        (new vertical-panel% [parent game-panel] [alignment '(center bottom)] [horiz-margin 5] [vert-margin 5]))
 
     ; Botón para regresar a la ventana de inicio
-    (new button% [parent button-panel]
+    (new button% [parent left-panel]
                  [label "Back"]
+                 [callback (λ (b e) (send game-window show #f) (send start-window show #t))])
+
+    (define center-panel
+        (new vertical-panel% [parent game-panel] [alignment '(center center)]))
+
+    (define init-board
+        (new message% [parent center-panel]
+                      [label (read-bitmap "src\\resources\\init-board.png")]))
+
+    (define right-panel
+        (new horizontal-panel% [parent game-panel] [alignment '(center top)] [horiz-margin 5] [vert-margin 5]))
+
+    ; Botón para regresar a la ventana de inicio
+    (new button% [parent right-panel]
+                 [label "Restart Match"]
                  [callback (λ (b e) (send game-window show #f) (send start-window show #t))])
 
     (send game-window show #t))
--- src/logic.rkt
+++ src/logic.rkt
@@ -1,16 +1,14 @@
-#lang racket
-;;Genera una fila de la matriz solicitada, con la cantidad de columnas especificada.
-(define (generaColumnas columnas lista)
-  (cond
-    ((equal? columnas 0) (cons lista '()))
-    (else (generaColumnas (- columnas 1) (cons 0 lista)))))
 
-;;Repite una fila encima de otra hasta saciar la cantidad de filas solicitada.
-(define (generaFilas filas lista matriz)
-  (cond
-    ((equal? filas 0) matriz)
-    (else (generaFilas (- filas 1) lista (append matriz lista)))))
+; Genera una fila de la matriz solicitada, con la cantidad de columnas especificada
+(define (generate-columns columns lst)
+  (cond ((equal? columns 0) (cons lst '()))
+        (else (generate-columns (- columns 1) (cons 0 lst)))))
 
-;;Combina el generaColumnas y el generaFilas para crear la matriz
-(define (generaMatriz filas columnas)
-  (generaFilas filas (generaColumnas columnas '()) '()))
+; Repite una fila encima de otra hasta saciar la cantidad de filas solicitada
+(define (generate-rows rows lst array)
+  (cond ((equal? rows 0) array)
+        (else (generate-rows (- rows 1) lst (append array lst)))))
+
+; Combina generate-columns y generate-rows para crear la matriz
+(define (generate-array rows columns)
+  (generate-rows rows (generate-columns columns '()) '()))
