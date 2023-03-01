70dcc8812aca37b5b9b2da6f5a5a1521d40362e6
GChacon8 <gabriel.chacon.alfaro@gmail.com>
2023-03-01 01:50:32 -0600

    Función move
    Se incorporó la función move que proyecta en la matriz de juego la jugada solicitada por algún jugador.

--- src/logic.rkt
+++ src/logic.rkt
@@ -14,14 +14,38 @@
   (generate-rows rows (generate-columns columns '()) '()))
 
 ; Revisa que no haya ceros en una lista especifica, en este caso representarian las filas
-(define (checkFullRow row)
+(define (isFullRow row)
   (cond
     ((null? row) #t)
     ((equal? (car row) 0) #f)
-    (else (checkFullRow (cdr row)))))
+    (else (isFullRow (cdr row)))))
 
 ; Utiliza el revisaLlenoFila para revisar que todas las filas de la matriz no tengan cero
-(define (checkFull matrix)
+(define (isFull matrix)
   (cond
-    ((equal? #t (checkFullRow (car matrix))) #t)
+    ((equal? #t (isFullRow (car matrix))) #t)
     (else #f)))
+
+; Revisa si el elemento especificado en una fila es igual a cero
+(define (isEmptyRow column row)
+  (cond
+    ((equal? (list-ref row (- column 1)) 0) #t)
+    (else #f)))
+
+; Cambia el valor en la posicion dada por otro elemento especificado y retorna la lista con el cambio
+(define (changeElement position element listB listR)
+  (cond
+    ((equal? position 1) (append listR (list element) (cdr listB)))
+    (else (changeElement (- position 1) element (cdr listB) (append listR (list(car listB)))))))
+
+; Retorna una lista sin su ultimo elemento
+(define (withoutTail lista)
+  (reverse(cdr(reverse lista))))
+
+; Realiza el movimiento especificado por un jugador en la columna dada, retornando la matriz con la jugada realizada
+(define (move column player matrix matrixP)
+  (cond
+    ((isEmptyRow column (list-ref matrix (- (length matrix) 1))) (append (withoutTail matrix) (list(changeElement column player (list-ref matrix (- (length matrix) 1)) '())) matrixP))
+    (else (move column player (withoutTail matrix) (append (list(list-ref matrix (- (length matrix) 1))) matrixP)))))
+
+;(move 2 1 '((0 0 0 0 0 0 0 0) (0 0 0 0 0 0 0 0) (0 0 0 0 0 0 0 0) (0 0 0 0 0 0 0 0) (0 0 0 0 0 0 0 0) (0 2 0 0 0 0 0 0) (0 2 0 0 0 0 0 0) (0 1 0 0 0 0 0 0)) '())
