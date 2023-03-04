064a1a7eeb342c5308f4a7f2a383a8bfcbd527b6
Jose Andrés Rodríguez Rojas <joseandres216@gmail.com>
2023-03-04 13:24:45 -0600

    Funciones candidatos, viabilidad y objetivo
    Se desarrollaron las funciones viabilidad y objetivo (se omite la funcion candidatos por recomendacion del profesor).

--- src/logic.rkt
+++ src/logic.rkt
@@ -1,3 +1,4 @@
+#lang racket
 
 ; Genera una fila de la matriz solicitada, con la cantidad de columnas especificada
 (define (generate-columns columns lst)
@@ -60,16 +61,6 @@
   (cond ((or (null? row) (> column (length row)) (= column 0)) null)
         ((> column 1) (getElementIn-Aux (- column 1) (cdr row)))
         (else (car row))))
-
-; Funcion candidatos.
-(define (candidatos matrix)
-  (candidatosAux matrix (length (car matrix)) (length matrix) '()))
-
-(define (candidatosAux matrix columns rows candidatesList)
-  (cond ((= columns 0) candidatesList)
-        ((= rows 0) (candidatosAux matrix (- columns 1) (length matrix) candidatesList))
-        ((= (getElementIn columns rows matrix) 0) (candidatosAux matrix (- columns 1) (length matrix) (append candidatesList (list (list columns rows)))))
-        (else (candidatosAux matrix columns (- rows 1) candidatesList))))
 
 ;Vertical
 (define (vertical-win-up column row matrix cont player flag)
@@ -153,5 +144,69 @@
   ((>= (+ (odd-diagonal-win-down column row matrix 0 player #t) (odd-diagonal-win-up column row matrix 0 player #t) 1) 4) #t)
   (else #f)))
 
-;(vertical-win 4 6 '((0 0 0 0 0 0 0 0) (0 0 0 0 0 0 0 0) (0 0 0 0 0 0 0 0) (0 0 0 0 0 0 0 0) (0 0 0 0 0 0 0 0) (0 0 2 1 0 0 0 0) (0 0 2 1 2 0 0 0) (0 0 1 1 2 0 0 0)) 1)
-(candidatos '((0 0 0 0 0 0 0 0) (0 0 0 0 0 0 0 0) (0 0 0 0 0 0 0 0) (0 0 0 0 0 0 0 0) (0 0 0 0 0 0 0 0) (0 0 2 1 0 0 0 0) (0 0 2 1 2 0 0 0) (0 0 1 1 2 0 0 0)))
+; Greeady Algorythm
+
+; Funcion Candidatos: Por sugerencia del profesor sobre el progreso realizado una vez hecha la consulta, se opto por omitir el paso de la funcion candidatos, ya que la funcion
+; viabilidad ya desempeña eficientemente el trabajo que deberian realizar ambas funciones por separado.
+
+; Funcion Viabilidad: Selecciona los candidatos reales.
+(define (viabilidad matrix playerUsingIA)
+  (viabilidadAux matrix playerUsingIA (length (car matrix)) (length matrix) '()))
+
+(define (viabilidadAux matrix playerUsingIA columns rows candidatesList)
+  (cond ((= columns 0) (objetivo matrix playerUsingIA candidatesList '()))
+        ((= rows 0) (viabilidadAux matrix (- columns 1) (length matrix) candidatesList))
+        ((= (getElementIn columns rows matrix) 0) (viabilidadAux matrix playerUsingIA (- columns 1) (length matrix) (append candidatesList (list (list columns rows)))))
+        (else (viabilidadAux matrix playerUsingIA columns (- rows 1) candidatesList))))
+
+; Funcion Objetivo: Asigna una valoracion a cada uno de los candidatos viables.
+(define (objetivo matrix playerUsingIA candidatesList candidatesRankedList)
+  (cond ((null? candidatesList) candidatesRankedList)
+        (else (objetivo matrix playerUsingIA (cdr candidatesList) (append candidatesRankedList (objetivoAux matrix (car candidatesList) playerUsingIA))))))
+
+(define (objetivoAux matrix candidateToRank playerUsingIA)
+  (cond ((connecting4 matrix (car candidateToRank) (car (cdr candidateToRank)) playerUsingIA) (list (append candidateToRank '(5))))
+        ((blockingRivalWin matrix (car candidateToRank) (car (cdr candidateToRank)) playerUsingIA) (list (append candidateToRank '(4))))
+        ((connecting3 matrix (car candidateToRank) (car (cdr candidateToRank)) playerUsingIA) (list (append candidateToRank '(3))))
+        ((connecting2 matrix (car candidateToRank) (car (cdr candidateToRank)) playerUsingIA) (list (append candidateToRank '(2))))
+        ((connecting1 matrix (car candidateToRank) (car (cdr candidateToRank)) playerUsingIA) (list (append candidateToRank '(1))))
+        (else (list candidateToRank 0))))
+
+(define (connecting1 matrix column row playerUsingIA)
+  (cond ((= (+ (vertical-win-down column row matrix 0 playerUsingIA #t) (vertical-win-up column row matrix 0 playerUsingIA #t) 1) 1) #t)
+        ((= (+ (horizontal-win-down column row matrix 0 playerUsingIA #t) (horizontal-win-up column row matrix 0 playerUsingIA #t) 1) 1) #t)
+        ((= (+ (pair-diagonal-win-down column row matrix 0 playerUsingIA #t) (pair-diagonal-win-up column row matrix 0 playerUsingIA #t) 1) 1) #t)
+        ((= (+ (odd-diagonal-win-down column row matrix 0 playerUsingIA #t) (odd-diagonal-win-up column row matrix 0 playerUsingIA #t) 1) 1) #t)
+        (else #f)))
+
+(define (connecting2 matrix column row playerUsingIA)
+  (cond ((= (+ (vertical-win-down column row matrix 0 playerUsingIA #t) (vertical-win-up column row matrix 0 playerUsingIA #t) 1) 2) #t)
+        ((= (+ (horizontal-win-down column row matrix 0 playerUsingIA #t) (horizontal-win-up column row matrix 0 playerUsingIA #t) 2) 1) #t)
+        ((= (+ (pair-diagonal-win-down column row matrix 0 playerUsingIA #t) (pair-diagonal-win-up column row matrix 0 playerUsingIA #t) 1) 2) #t)
+        ((= (+ (odd-diagonal-win-down column row matrix 0 playerUsingIA #t) (odd-diagonal-win-up column row matrix 0 playerUsingIA #t) 1) 2) #t)
+        (else #f)))
+
+(define (connecting3 matrix column row playerUsingIA)
+  (cond ((= (+ (vertical-win-down column row matrix 0 playerUsingIA #t) (vertical-win-up column row matrix 0 playerUsingIA #t) 1) 3) #t)
+        ((= (+ (horizontal-win-down column row matrix 0 playerUsingIA #t) (horizontal-win-up column row matrix 0 playerUsingIA #t) 1) 3) #t)
+        ((= (+ (pair-diagonal-win-down column row matrix 0 playerUsingIA #t) (pair-diagonal-win-up column row matrix 0 playerUsingIA #t) 1) 3) #t)
+        ((= (+ (odd-diagonal-win-down column row matrix 0 playerUsingIA #t) (odd-diagonal-win-up column row matrix 0 playerUsingIA #t) 1) 3) #t)
+        (else #f)))
+
+(define (blockingRivalWin matrix column row playerUsingIA)
+  (cond ((= playerUsingIA 1) (blockingRivalWinAux matrix column row 2))
+        (else (blockingRivalWinAux matrix column row 1))))
+
+(define (blockingRivalWinAux matrix column row playerUsingRival)
+  (cond ((>= (+ (vertical-win-down column row matrix 0 playerUsingRival #t) (vertical-win-up column row matrix 0 playerUsingRival #t) 1) 4) #t)
+        ((>= (+ (horizontal-win-down column row matrix 0 playerUsingRival #t) (horizontal-win-up column row matrix 0 playerUsingRival #t) 1) 4) #t)
+        ((>= (+ (pair-diagonal-win-down column row matrix 0 playerUsingRival #t) (pair-diagonal-win-up column row matrix 0 playerUsingRival #t) 1) 4) #t)
+        ((>= (+ (odd-diagonal-win-down column row matrix 0 playerUsingRival #t) (odd-diagonal-win-up column row matrix 0 playerUsingRival #t) 1) 4) #t)
+        (else #f)))
+
+(define (connecting4 matrix column row playerUsingIA)
+  (cond ((>= (+ (vertical-win-down column row matrix 0 playerUsingIA #t) (vertical-win-up column row matrix 0 playerUsingIA #t) 1) 4) #t)
+        ((>= (+ (horizontal-win-down column row matrix 0 playerUsingIA #t) (horizontal-win-up column row matrix 0 playerUsingIA #t) 1) 4) #t)
+        ((>= (+ (pair-diagonal-win-down column row matrix 0 playerUsingIA #t) (pair-diagonal-win-up column row matrix 0 playerUsingIA #t) 1) 4) #t)
+        ((>= (+ (odd-diagonal-win-down column row matrix 0 playerUsingIA #t) (odd-diagonal-win-up column row matrix 0 playerUsingIA #t) 1) 4) #t)
+        (else #f)))
